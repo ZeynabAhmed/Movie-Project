42c8a950d4985bc5783f40bd77ad5316bda1cac9
Mostafa Samir <mostafasamir@Mostafas-Mac.local>
2020-01-06 19:56:07 +0200

    Initial Commit

---
+++ swift_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift_Project
+//
+//  Created by Mostafa Samir on 1/6/20.
+//  Copyright © 2020 Mostafa Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
